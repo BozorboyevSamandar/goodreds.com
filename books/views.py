32af3e86b2a3faa8ebcc40bd1763e2f7450a5140
BS_CODER <92523813+BozorboyevSamandar@users.noreply.github.com>
2022-05-25 13:20:20 +0500

    add Generic View and Book Cover img model

--- books/views.py
+++ books/views.py
@@ -1,19 +1,28 @@
-from django.shortcuts import render
-from django.views import View
+from django.views.generic import ListView, DetailView
+
 from books.models import Book
 
 
 # Create your views here.
 
+class BookView(ListView):
+    template_name = "books/list.html"
+    queryset = Book.objects.all()
+    context_object_name = "books"
 
-class BookView(View):
-    def get(self, request):
-        books = Book.objects.all()
-        return render(request, "books/list.html", {"books": books})
 
+# class BookView(View):
+#     def get(self, request):
+#         books = Book.objects.all()
+#         return render(request, "books/list.html", {"books": books})
 
-class BookDetailView(View):
-    def get(self, request, id):
-        book = Book.objects.get(id=id)
+class BookDetailView(DetailView):
+    template_name = "books/detail.html"
+    pk_url_kwarg = "id"
+    model = Book
 
-        return render(request, "books/detail.html", {"book": book})
+# class BookDetailView(View):
+#     def get(self, request, id):
+#         book = Book.objects.get(id=id)
+#
+#         return render(request, "books/detail.html", {"book": book})
