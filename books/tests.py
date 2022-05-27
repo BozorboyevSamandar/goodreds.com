4316033735f409ecf21e329049890c85343edb55
BS_CODER <92523813+BozorboyevSamandar@users.noreply.github.com>
2022-05-27 21:35:41 +0500

    add commit

--- books/tests.py
+++ books/tests.py
@@ -17,12 +17,13 @@ class BookTaseCase(TestCase):
         book2 = Book.objects.create(title="Book2", description="Description2", isbn="13232112")
         book3 = Book.objects.create(title="Book3", description="Description3", isbn="13232112")
 
-        response = self.client.get(reverse("books:list"))
+        response = self.client.get(reverse("books:list") + "?page_size=2")
 
         for book in [book1, book2]:
             self.assertContains(response, book.title)
+            self.assertNotContains(response, book3.title)
 
-        response = self.client.get(reverse("books:list") + "?page=2")
+        response = self.client.get(reverse("books:list") + "?page=2&page_size=2")
 
         self.assertContains(response, book3.title)
 
@@ -33,3 +34,13 @@ class BookTaseCase(TestCase):
 
         self.assertContains(response, book.title)
         self.assertContains(response, book.description)
+
+    def test_search_book(self):
+        book1 = Book.objects.create(title="sport", description="Description1", isbn="13232112")
+        book2 = Book.objects.create(title="Book2", description="Description2", isbn="13232112")
+        book3 = Book.objects.create(title="Book3", description="Description3", isbn="13232112")
+
+        response = self.client.get(reverse("books:list")+"?q=sport")
+        self.assertContains(response, book1.title)
+        self.assertNotContains(response, book2.title)
+        self.assertNotContains(response, book3.title)
